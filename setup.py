60a3da54e8c4806237b22adffe0cc05fcf5cd667
Tian Jin <tian.jin1@ibm.com>
2017-10-23 00:12:07 -0400

    update package info

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup
 
 setup(name='onnxtf',
       version='0.1',
-      description='Tensorflow backend for ONNX',
+      description='Tensorflow backend for ONNX.',
       url='TBD',
-      author='IBM',
-      author_email='TBD',
+      author='Arpith Jacob, Tian Jin, Gheorghe-teod Bercea',
+      author_email='tian.jin1@ibm.com',
       license='TBD',
       packages=['onnxtf'],
       zip_safe=False)
