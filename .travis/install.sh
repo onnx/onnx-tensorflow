2930f38ba16e414e97a454be60a7f83f6b533fa1
Tian Jin <tian.jin1@ibm.com>
2017-11-16 11:30:05 -0500

    fix onnx install

--- .travis/install.sh
+++ .travis/install.sh
@@ -14,4 +14,4 @@ ccache -s
 # install onnx
 onnx_dir="$workdir/onnx"
 mkdir -p $onnx_dir
-cd "$onnx_dir" && git clone --recursive git://github.com/onnx/onnx.git && pip install onnx
+cd "$onnx_dir" && git clone --recursive git://github.com/onnx/onnx.git && pip install -e onnx
