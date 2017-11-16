6117a8ed42b09bb3b42d6909a3dde6a2f59a5013
Tian Jin <tian.jin1@ibm.com>
2017-11-16 10:35:06 -0500

    remove version specification of onnx from setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='onnx-tf',
       version='0.1.1',
       description='Tensorflow backend for ONNX.',
-      install_requires=['onnx==0.2', 'tensorflow', 'numpy'],
+      install_requires=['onnx', 'tensorflow', 'numpy'],
       url='https://github.com/tjingrant',
       author='Arpith Jacob, Tian Jin, Gheorghe-Teodor Bercea',
       author_email='tian.jin1@ibm.com',
