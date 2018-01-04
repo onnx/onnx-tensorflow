9191b77292a2ac56cb5af3148f4b1800388b5a48
LucasMahieu <lmahieu@kalray.eu>
2018-01-04 13:46:49 +0100

    Front-end tests automatically running on all possible devices and with all possible data formats

--- test/frontend/test_node.py
+++ test/frontend/test_node.py
@@ -13,6 +13,18 @@ from onnx import helper
 # for testing
 from onnx_tf.backend import prepare
 
+from tensorflow.python.client import device_lib
+DEVISE_TO_TEST = ["CPU", "CUDA"]
+
+def supports_device(device):
+  if device == "CUDA":
+    local_device_protos = device_lib.list_local_devices()
+    return len([x.name for x in
+                local_device_protos if x.device_type == 'GPU']) > 0
+  elif device == "CPU":
+    return True
+  return False
+
 def get_node_by_name(nodes, name):
   for node in nodes:
     if node.name == name:
@@ -38,53 +50,59 @@ class TestNode(unittest.TestCase):
   pass
 
 def create_test(test_data):
-  test_option = test_data[7] if len(test_data) > 7 else {}
+  test_option = test_data[5] if len(test_data) > 5 else {}
 
   def do_test_expected(self):
     tf_op = test_data[1]
     output_name = test_data[2]
     inputs = test_data[3]
     attrs = test_data[4]
-    device = test_data[5]
-    channel_last = test_data[6]
+    for device in DEVISE_TO_TEST:
+      if not supports_device(device):
+        # This case can not be tester on this device
+        continue
+      for channel_last in [True, False]:
+        if channel_last in [False] and device in ["CPU"]:
+          # This combination is incompatible
+          continue
 
-    # Now construct input feed dict
-    # keyed by input name
-    onnx_feed_dict = {}
-    # keyed by placeholder op
-    tf_feed_dict = {}
-    tf_param_list = []
-    for idx, input_tensor in enumerate(inputs):
-      if type(input_tensor) is np.ndarray:
-        placeholder = tf.placeholder(input_tensor.dtype,
-                                     shape=input_tensor.shape,
-                                     name="in_" + str(idx))
-        onnx_feed_dict["in_" + str(idx)] = input_tensor
-        # TF have to get input in format : NHWC
-        tf_feed_dict[placeholder] = input_tensor
-        tf_param_list.append(placeholder)
-      else:
-        tf_param_list.append(input_tensor)
-    test_op = tf_op(*tf_param_list, **attrs)
-    tf_graph = tf.get_default_graph().as_graph_def(add_shapes=True)
-    # Construct onnx graph, run with backend.
-    output_node = get_node_by_name(tf_graph.node, output_name)
-    onnx_graph = convert_graph(tf_graph, output_node, device=device,
-            channel_last=channel_last)
-    onnx_model = helper.make_model(onnx_graph)
-    backend_rep = prepare(onnx_model, device=device, channel_last=channel_last)
-    backend_output = backend_rep.run(onnx_feed_dict)[output_name]
+        # Now construct input feed dict
+        # keyed by input name
+        onnx_feed_dict = {}
+        # keyed by placeholder op
+        tf_feed_dict = {}
+        tf_param_list = []
+        for idx, input_tensor in enumerate(inputs):
+          if type(input_tensor) is np.ndarray:
+            placeholder = tf.placeholder(input_tensor.dtype,
+                                         shape=input_tensor.shape,
+                                         name="in_" + str(idx))
+            onnx_feed_dict["in_" + str(idx)] = input_tensor
+            # TF have to get input in format : NHWC
+            tf_feed_dict[placeholder] = input_tensor
+            tf_param_list.append(placeholder)
+          else:
+            tf_param_list.append(input_tensor)
+        test_op = tf_op(*tf_param_list, **attrs)
+        tf_graph = tf.get_default_graph().as_graph_def(add_shapes=True)
+        # Construct onnx graph, run with backend.
+        output_node = get_node_by_name(tf_graph.node, output_name)
+        onnx_graph = convert_graph(tf_graph, output_node, device=device,
+                channel_last=channel_last)
+        onnx_model = helper.make_model(onnx_graph)
+        backend_rep = prepare(onnx_model, device=device, channel_last=channel_last)
+        backend_output = backend_rep.run(onnx_feed_dict)[output_name]
 
-    with tf.Session() as sess:
-      tf_output = sess.run(test_op, tf_feed_dict)
-    tf.reset_default_graph()
+        with tf.Session() as sess:
+          tf_output = sess.run(test_op, tf_feed_dict)
+        tf.reset_default_graph()
 
-    # skip comparison if test_option specifies that
-    # the test is call only.
-    if (test_option.get("call_only", False)):
-      return
+        # skip comparison if test_option specifies that
+        # the test is call only.
+        if (test_option.get("call_only", False)):
+          return
 
-    np.testing.assert_allclose(backend_output, tf_output)
+        np.testing.assert_allclose(backend_output, tf_output)
 
   return do_test_expected
 
@@ -94,48 +112,48 @@ def create_test(test_data):
 # in the sense that numpy array are passed in via tf.placeholder
 # whereas python arrays are passed in as constants.
 test_cases = [
-("test_relu", tf.nn.relu, "Relu", [get_rnd([10, 10])], {}, 'CPU', True),
-("test_or", tf.logical_or, "LogicalOr", [get_rnd([10, 10], dtype=np.bool_), get_rnd([10, 10], dtype=np.bool_)], {}, 'CPU', True),
-("test_pow", tf.pow, "Pow", [get_rnd([10, 10]), get_rnd([10, 10])], {}, 'CPU', True),
-("test_pad", tf.pad, "Pad", [get_rnd([2, 3]), [[1, 1,], [2, 2]]], {"mode": "constant"}, 'CPU', True),
-("test_pad", tf.pad, "Pad", [get_rnd([10, 2, 2, 3]), [[0,0], [2, 2], [2, 2], [0,0]]], {"mode": "constant"}, 'CPU', True),
-("test_random_normal", tf.random_normal, "random_normal/RandomStandardNormal", [], {"shape": [1, 100, 100, 1], "mean": 0.0, "stddev": 1.0, "dtype": tf.float32, "seed": 42}, 'CPU', True, {"call_only": True}),
-("test_random_normal", tf.random_normal, "random_normal/RandomStandardNormal", [], {"shape": [100, 100], "mean": 0.0, "stddev": 1.0, "dtype": tf.float32, "seed": 42}, 'CPU', True, {"call_only": True}),
-("test_random_uniform", tf.random_uniform, "random_uniform", [], {"shape": [100, 100], "minval": 0.0, "maxval": 1.0, "dtype": tf.float32, "seed": 42}, 'CPU', True, {"call_only": True}),
-("test_random_uniform", tf.random_uniform, "random_uniform", [], {"shape": [1, 42, 42, 3], "minval": 0.0, "maxval": 1.0, "dtype": tf.float32, "seed": 42}, 'CPU', True, {"call_only": True}),
-("test_reciprocal", tf.reciprocal, "Reciprocal", [get_rnd([10, 10])], {}, 'CPU', True),
-("test_reduce_max", tf.reduce_max, "Max", [get_rnd([10, 10, 10, 10])], {"axis": [0, 1, 2, 3], "keep_dims": True}, 'CPU', True),
-("test_reduce_max", tf.reduce_max, "Max", [get_rnd([10, 10])], {"keep_dims": True}, 'CPU', True),
-("test_reduce_mean", tf.reduce_mean, "Mean", [get_rnd([10, 10])], {"keep_dims": True}, 'CPU', True),
-("test_reduce_min", tf.reduce_min, "Min", [get_rnd([10, 10])], {"keep_dims": True}, 'CPU', True),
-("test_reduce_prod", tf.reduce_prod, "Prod", [get_rnd([10, 10])], {"keep_dims": True}, 'CPU', True),
-("test_reduce_prod", tf.reduce_prod, "Prod", [get_rnd([1, 10, 10, 3])], {"axis": [1, 2], "keep_dims": True}, 'CPU', True),
-("test_reduce_sum", tf.reduce_sum, "Sum", [get_rnd([10, 10])], {"keep_dims": True}, 'CPU', True),
-("test_reduce_sum", tf.reduce_sum, "Sum", [get_rnd([1, 10, 10, 3])], {"axis": [1, 2], "keep_dims": True}, 'CPU', True),
-("test_reshape", tf.reshape, "Reshape", [get_rnd([1, 16, 16, 3]), [1, 16*16, 3]], {}, 'CPU', True),
-("test_reshape", tf.reshape, "Reshape", [get_rnd([1, 16, 16, 3]), [3, 16*16]], {}, 'CPU', True),
-("test_reshape", tf.reshape, "Reshape", [get_rnd([1, 16, 16, 3]), [3, 16, 16]], {}, 'CPU', True),
-("test_reshape", tf.reshape, "Reshape", [get_rnd([1, 16, 16, 1]), [16, 16]], {}, 'CPU', True),
-("test_reshape", tf.reshape, "Reshape", [get_rnd([10, 10]), [4, 25]], {}, 'CPU', True),
-("test_sigmoid", tf.sigmoid, "Sigmoid", [get_rnd([10, 10])], {}, 'CPU', True),
-("test_split", tf.split, "split", [get_rnd([1, 10, 10, 3]), [1, 5, 5, 1]], {}, 'CPU', True),
-("test_split", tf.split, "split", [get_rnd([10, 10]), [5, 5]], {}, 'CPU', True),
-("test_sqrt", tf.sqrt, "Sqrt", [get_rnd([10, 10])], {}, 'CPU', True),
-("test_squeeze", tf.squeeze, "Squeeze", [get_rnd([1, 1, 1, 1])], {"axis":[0, 3, 2, 1]}, 'CPU', True),
-("test_squeeze", tf.squeeze, "Squeeze", [get_rnd([1, 10, 10])], {"axis":[0]}, 'CPU', True),
-("test_squeeze", tf.squeeze, "Squeeze", [get_rnd([1, 10, 10, 1])], {"axis":[0, 3]}, 'CPU', True),
-("test_squeeze", tf.squeeze, "Squeeze", [get_rnd([10, 1, 1, 3])], {"axis":[1, 2]}, 'CPU', True),
-("test_subtract", tf.subtract, "Sub", [get_rnd([10, 10]), get_rnd([10, 10])], {}, 'CPU', True),
-("test_tanh", tf.tanh, "Tanh", [get_rnd([10, 10])], {}, 'CPU', True),
-("test_xor", tf.logical_xor, "LogicalXor", [get_rnd([10, 10], dtype=np.bool_), get_rnd([10, 10], dtype=np.bool_)], {}, 'CPU', True),
-("test_transpose", tf.transpose, "transpose", [get_rnd([1, 3, 2, 10])], {"perm":[2, 3, 1, 0]}, 'CPU', True),
-("test_transpose", tf.transpose, "transpose", [get_rnd([1, 16, 16, 3])], {"perm":[0, 3, 1, 2]}, 'CPU', True),
-("test_transpose", tf.transpose, "transpose", [get_rnd([1, 2, 2, 10])], {"perm":[3, 1, 2, 0]}, 'CPU', True),
-("test_transpose", tf.transpose, "transpose", [get_rnd([2, 10])], {"perm":[1, 0]}, 'CPU', True),
-("test_concat", tf.concat, "concat", [[get_rnd([2, 1]), get_rnd([2, 2]), get_rnd([2, 1])], 1], {}, 'CPU', True),
-("test_concat", tf.concat, "concat", [[get_rnd([2, 1, 2]), get_rnd([2, 2, 2]), get_rnd([2, 1, 2])], 1], {}, 'CPU', True),
-("test_concat", tf.concat, "concat", [[get_rnd([1, 10, 10, 1]), get_rnd([1, 10, 10, 1]), get_rnd([1, 10, 10, 1])], 3], {}, 'CPU', True),
-("test_concat", tf.concat, "concat", [[get_rnd([1, 10, 10, 1]), get_rnd([1, 10, 10, 1]), get_rnd([1, 10, 10, 1])], 0], {}, 'CPU', True),
+("test_relu", tf.nn.relu, "Relu", [get_rnd([10, 10])], {}),
+("test_or", tf.logical_or, "LogicalOr", [get_rnd([10, 10], dtype=np.bool_), get_rnd([10, 10], dtype=np.bool_)], {}),
+("test_pow", tf.pow, "Pow", [get_rnd([10, 10]), get_rnd([10, 10])], {}),
+("test_pad", tf.pad, "Pad", [get_rnd([2, 3]), [[1, 1,], [2, 2]]], {"mode": "constant"}),
+("test_pad", tf.pad, "Pad", [get_rnd([10, 2, 2, 3]), [[0,0], [2, 2], [2, 2], [0,0]]], {"mode": "constant"}),
+("test_random_normal", tf.random_normal, "random_normal/RandomStandardNormal", [], {"shape": [1, 100, 100, 1], "mean": 0.0, "stddev": 1.0, "dtype": tf.float32, "seed": 42}, {"call_only": True}),
+("test_random_normal", tf.random_normal, "random_normal/RandomStandardNormal", [], {"shape": [100, 100], "mean": 0.0, "stddev": 1.0, "dtype": tf.float32, "seed": 42}, {"call_only": True}),
+("test_random_uniform", tf.random_uniform, "random_uniform", [], {"shape": [100, 100], "minval": 0.0, "maxval": 1.0, "dtype": tf.float32, "seed": 42}, {"call_only": True}),
+("test_random_uniform", tf.random_uniform, "random_uniform", [], {"shape": [1, 42, 42, 3], "minval": 0.0, "maxval": 1.0, "dtype": tf.float32, "seed": 42}, {"call_only": True}),
+("test_reciprocal", tf.reciprocal, "Reciprocal", [get_rnd([10, 10])], {}),
+("test_reduce_max", tf.reduce_max, "Max", [get_rnd([10, 10, 10, 10])], {"axis": [0, 1, 2, 3], "keep_dims": True}),
+("test_reduce_max", tf.reduce_max, "Max", [get_rnd([10, 10])], {"keep_dims": True}),
+("test_reduce_mean", tf.reduce_mean, "Mean", [get_rnd([10, 10])], {"keep_dims": True}),
+("test_reduce_min", tf.reduce_min, "Min", [get_rnd([10, 10])], {"keep_dims": True}),
+("test_reduce_prod", tf.reduce_prod, "Prod", [get_rnd([10, 10])], {"keep_dims": True}),
+("test_reduce_prod", tf.reduce_prod, "Prod", [get_rnd([1, 10, 10, 3])], {"axis": [1, 2], "keep_dims": True}),
+("test_reduce_sum", tf.reduce_sum, "Sum", [get_rnd([10, 10])], {"keep_dims": True}),
+("test_reduce_sum", tf.reduce_sum, "Sum", [get_rnd([1, 10, 10, 3])], {"axis": [1, 2], "keep_dims": True}),
+("test_reshape", tf.reshape, "Reshape", [get_rnd([1, 16, 16, 3]), [1, 16*16, 3]], {}),
+("test_reshape", tf.reshape, "Reshape", [get_rnd([1, 16, 16, 3]), [3, 16*16]], {}),
+("test_reshape", tf.reshape, "Reshape", [get_rnd([1, 16, 16, 3]), [3, 16, 16]], {}),
+("test_reshape", tf.reshape, "Reshape", [get_rnd([1, 16, 16, 1]), [16, 16]], {}),
+("test_reshape", tf.reshape, "Reshape", [get_rnd([10, 10]), [4, 25]], {}),
+("test_sigmoid", tf.sigmoid, "Sigmoid", [get_rnd([10, 10])], {}),
+("test_split", tf.split, "split", [get_rnd([1, 10, 10, 3]), [1, 5, 5, 1]], {}),
+("test_split", tf.split, "split", [get_rnd([10, 10]), [5, 5]], {}),
+("test_sqrt", tf.sqrt, "Sqrt", [get_rnd([10, 10])], {}),
+("test_squeeze", tf.squeeze, "Squeeze", [get_rnd([1, 1, 1, 1])], {"axis":[0, 3, 2, 1]}),
+("test_squeeze", tf.squeeze, "Squeeze", [get_rnd([1, 10, 10])], {"axis":[0]}),
+("test_squeeze", tf.squeeze, "Squeeze", [get_rnd([1, 10, 10, 1])], {"axis":[0, 3]}),
+("test_squeeze", tf.squeeze, "Squeeze", [get_rnd([10, 1, 1, 3])], {"axis":[1, 2]}),
+("test_subtract", tf.subtract, "Sub", [get_rnd([10, 10]), get_rnd([10, 10])], {}),
+("test_tanh", tf.tanh, "Tanh", [get_rnd([10, 10])], {}),
+("test_xor", tf.logical_xor, "LogicalXor", [get_rnd([10, 10], dtype=np.bool_), get_rnd([10, 10], dtype=np.bool_)], {}),
+("test_transpose", tf.transpose, "transpose", [get_rnd([1, 3, 2, 10])], {"perm":[2, 3, 1, 0]}),
+("test_transpose", tf.transpose, "transpose", [get_rnd([1, 16, 16, 3])], {"perm":[0, 3, 1, 2]}),
+("test_transpose", tf.transpose, "transpose", [get_rnd([1, 2, 2, 10])], {"perm":[3, 1, 2, 0]}),
+("test_transpose", tf.transpose, "transpose", [get_rnd([2, 10])], {"perm":[1, 0]}),
+("test_concat", tf.concat, "concat", [[get_rnd([2, 1]), get_rnd([2, 2]), get_rnd([2, 1])], 1], {}),
+("test_concat", tf.concat, "concat", [[get_rnd([2, 1, 2]), get_rnd([2, 2, 2]), get_rnd([2, 1, 2])], 1], {}),
+("test_concat", tf.concat, "concat", [[get_rnd([1, 10, 10, 1]), get_rnd([1, 10, 10, 1]), get_rnd([1, 10, 10, 1])], 3], {}),
+("test_concat", tf.concat, "concat", [[get_rnd([1, 10, 10, 1]), get_rnd([1, 10, 10, 1]), get_rnd([1, 10, 10, 1])], 0], {}),
 ]
 
 for k, val in enumerate(test_cases):
