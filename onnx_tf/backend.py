4e586749487ef41c1a98cfe6bbd805be5f6a8fe8
Wenhao Hu <fumihwh@gmail.com>
2018-03-01 23:54:44 +0900

    add avg pool 1d support

--- onnx_tf/backend.py
+++ onnx_tf/backend.py
@@ -365,47 +365,34 @@ class TensorflowBackend(Backend):
 
   @classmethod
   def _compatibility_avg_pool(cls, node, input_dict):
-    def py_pool(x, auto_pad, kernel_shape, strides_shape, pads):
-      def _get_pad_shape(auto_pad, input_spatial_shape, kernel_spatial_shape, strides_spatial, output_spatial_shape):
-        pad_shape = [0] * len(input_spatial_shape)
-        if auto_pad in ("SAME_UPPER", "SAME_LOWER"):
-          for i in range(len(input_spatial_shape)):
-            pad_shape[i] = (output_spatial_shape[i] - 1) * strides_spatial[i] + kernel_spatial_shape[i] - \
-                           input_spatial_shape[i]
-        elif auto_pad in ("VALID", ""):
-          pass
-        return pad_shape
+    def _get_pad_shape(auto_pad, input_spatial_shape, kernel_spatial_shape, strides_spatial, output_spatial_shape):
+      pad_shape = [0] * len(input_spatial_shape)
+      if auto_pad in ("SAME_UPPER", "SAME_LOWER"):
+        for i in range(len(input_spatial_shape)):
+          pad_shape[i] = (output_spatial_shape[i] - 1) * strides_spatial[i] + kernel_spatial_shape[i] - \
+                         input_spatial_shape[i]
+      elif auto_pad in ("VALID", ""):
+        pass
+      return pad_shape
 
-      def _get_output_shape(auto_pad, input_spatial_shape, kernel_spatial_shape, strides_spatial):
-        out_shape = [0] * len(input_spatial_shape)
-        if auto_pad in ("SAME_UPPER", "SAME_LOWER"):
-          for i in range(len(input_spatial_shape)):
-            out_shape[i] = int(np.ceil(input_spatial_shape[i] / strides_spatial[i]))
-        elif auto_pad in ("VALID", ""):
-          for i in range(len(input_spatial_shape)):
-            out_shape[i] = int(
-              np.ceil((input_spatial_shape[i] - (kernel_spatial_shape[i] - 1)) / strides_spatial[i]))
-        return out_shape
+    def _get_output_shape(auto_pad, input_spatial_shape, kernel_spatial_shape, strides_spatial):
+      out_shape = [0] * len(input_spatial_shape)
+      if auto_pad in ("SAME_UPPER", "SAME_LOWER"):
+        for i in range(len(input_spatial_shape)):
+          out_shape[i] = int(np.ceil(input_spatial_shape[i] / strides_spatial[i]))
+      elif auto_pad in ("VALID", ""):
+        for i in range(len(input_spatial_shape)):
+          out_shape[i] = int(
+            np.ceil((input_spatial_shape[i] - (kernel_spatial_shape[i] - 1)) / strides_spatial[i]))
+      return out_shape
 
-
+    def py_pool(x, kernel_shape, strides_shape, pads, out_shape, pad_shape):
       x_shape = np.shape(x)
       spatial_size = len(x_shape[2:])
-      auto_pad = auto_pad.decode("utf-8")
-
-      if auto_pad == "SAME_LOWER":
-        out_shape = _get_output_shape(auto_pad, x_shape[2:], kernel_shape, strides_shape)
-        pad_shape = _get_pad_shape(auto_pad, x_shape[2:], kernel_shape, strides_shape, out_shape)
-        for i in range(spatial_size):
-          pads[i + spatial_size] = pad_shape[i] // 2
-          pads[i] = pad_shape[i] - pads[i + spatial_size]
-      elif auto_pad == "":
-        pad_shape = [pads[i] + pads[i + spatial_size] for i in range(spatial_size)]
-        out_shape = _get_output_shape(auto_pad, np.add(x_shape[2:], pad_shape), kernel_shape, strides_shape)
-
       pad_attr = [(0, 0), (0, 0)] + [(pads[i], pads[i + spatial_size]) for i in range(spatial_size)]
       padded = np.pad(x, pad_attr, mode="constant", constant_values=np.nan)
 
-      y = np.zeros([x_shape[0], x_shape[1]] + out_shape)
+      y = np.zeros([x_shape[0], x_shape[1]] + list(out_shape))
 
       for shape in itertools.product(range(x_shape[0]),
                                      range(x_shape[1]),
@@ -413,7 +400,10 @@ class TensorflowBackend(Backend):
                                        int((x_shape[i + 2] + pad_shape[i] - kernel_shape[i]) / strides_shape[i] + 1))
                                        for i in range(spatial_size)]):
         window = padded[shape[0], shape[1]]
-        if spatial_size == 2:
+        if spatial_size == 1:
+          window = window[
+                   strides_shape[0] * shape[2]:strides_shape[0] * shape[2] + kernel_shape[0]]
+        elif spatial_size == 2:
           window = window[
                    strides_shape[0] * shape[2]:strides_shape[0] * shape[2] + kernel_shape[0],
                    strides_shape[1] * shape[3]:strides_shape[1] * shape[3] + kernel_shape[1]]
@@ -423,7 +413,9 @@ class TensorflowBackend(Backend):
                    strides_shape[1] * shape[3]:strides_shape[1] * shape[3] + kernel_shape[1],
                    strides_shape[2] * shape[4]:strides_shape[2] * shape[4] + kernel_shape[2]]
         average = np.average(window[np.where(~np.isnan(window))])
-        if spatial_size == 2:
+        if spatial_size == 1:
+          y[shape[0], shape[1], shape[2]] = average
+        elif spatial_size == 2:
           y[shape[0], shape[1], shape[2], shape[3]] = average
         elif spatial_size == 3:
           y[shape[0], shape[1], shape[2], shape[3], shape[4]] = average
@@ -437,7 +429,18 @@ class TensorflowBackend(Backend):
     pads = node.attrs.get("pads", [0] * spatial_size * 2)
     auto_pad = node.attrs.get("auto_pad", "")
 
-    pooled = tf.py_func(py_pool, [x, auto_pad, kernel_shape, strides_shape, pads], tf.float32)
+    if auto_pad == "SAME_LOWER":
+      out_shape = _get_output_shape(auto_pad, x_shape[2:], kernel_shape, strides_shape)
+      pad_shape = _get_pad_shape(auto_pad, x_shape[2:], kernel_shape, strides_shape, out_shape)
+      for i in range(spatial_size):
+        pads[i + spatial_size] = pad_shape[i] // 2
+        pads[i] = pad_shape[i] - pads[i + spatial_size]
+    elif auto_pad == "":
+      pad_shape = [pads[i] + pads[i + spatial_size] for i in range(spatial_size)]
+      out_shape = _get_output_shape(auto_pad, np.add(x_shape[2:], pad_shape), kernel_shape, strides_shape)
+
+    pooled = tf.py_func(py_pool, [x, kernel_shape, strides_shape, pads, out_shape, pad_shape], tf.float32)
+    pooled.set_shape(x_shape[0:2] + out_shape)
     return [pooled]
 
   @classmethod
