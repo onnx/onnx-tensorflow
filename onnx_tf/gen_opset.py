82d5d43efd86eed1c423cf457b1e82629c34134d
Tian Jin <tian.jin1@ibm.com>
2018-03-19 22:37:18 -0400

    add trivial case

--- onnx_tf/gen_opset.py
+++ onnx_tf/gen_opset.py
@@ -12,6 +12,7 @@ import pprint
 
 from onnx_tf.common import (
   op_name_to_lower,
+  ONNX_OP_TO_TF_OP,
 )
 
 def main():
@@ -25,20 +26,22 @@ def main():
     while True:
         try:
             backend = (importlib.import_module('backend_v{}'
-                        .format(version))
-                        .TensorflowBackend)
+                                .format(version))
+                                .TensorflowBackend)
         except:
             break
 
         for schema in defs.get_all_schemas():
             op_name = op_name_to_lower(schema.name)
-            if hasattr(backend, 'handle_' + op_name):
+            has_handler = hasattr(backend, 'handle_' + op_name)
+            is_trivial = op_name in ONNX_OP_TO_TF_OP.keys()
+            if has_handler or is_trivial:
                 opset_dict[op_name].append(version)
         version += 1
 
     with open('opset_version.py', 'w') as version_file:
         pp = pprint.PrettyPrinter(indent=4)
-        version_file.write("opset_version = " + pp.pformat(opset_dict))
+        version_file.write("backend_opset_version = " + pp.pformat(opset_dict))
 
 
 if __name__ == '__main__':
