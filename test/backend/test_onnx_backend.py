80810c65f8f9a96062402c5a3086f67c55d06212
Wenhao Hu <fumihwh@gmail.com>
2018-03-31 00:14:59 +0900

    remove code for skipping AvgPool test (#103)

--- test/backend/test_onnx_backend.py
+++ test/backend/test_onnx_backend.py
@@ -17,9 +17,6 @@ backend_test = onnx.backend.test.BackendTest(tf_backend, __name__)
 # https://github.com/onnx/onnx/issues/349
 backend_test.exclude(r'[a-z,_]*GLU[a-z,_]*')
 
-# https://github.com/onnx/onnx/issues/576
-backend_test.exclude(r'[a-z,_]*AvgPool[a-z,_]*')
-
 # TF does not support dialation and strides at the same time:
 # Will produce strides > 1 not supported in conjunction with dilation_rate > 1
 backend_test.exclude(r'[a-z,_]*dilated_strided[a-z,_]*')
