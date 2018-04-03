b4f79349be45e8923db72e68c29e6d31e08dc164
Wenhao Hu <fumihwh@gmail.com>
2018-04-03 16:32:40 +0900

    add multiple domains support

--- onnx_tf/frontend.py
+++ onnx_tf/frontend.py
@@ -121,7 +121,7 @@ class TensorflowFrontendBase(object):
   def tensorflow_graph_to_onnx_graph(cls,
                                      graph_def,
                                      output,
-                                     opset={"": 0},
+                                     opset=(("", 0),),
                                      name="graph"):
     """Converts a Tensorflow Graph Proto to an ONNX graph
 
@@ -131,7 +131,7 @@ class TensorflowFrontendBase(object):
     :param graph_def: Tensorflow Graph Proto object.
     :param output: A Tensorflow NodeDef object specifying which node
       to be taken as output of the ONNX graph.
-    :param opset: Opset, which should be {str domain: int version number, }.
+    :param opset: Opset, which should be ((str domain: int version number),).
     :param name: The name of the output ONNX Graph.
 
     :returns: The equivalent ONNX Graph Proto object.
@@ -188,22 +188,35 @@ class TensorflowFrontendBase(object):
                 data_type=node.attr["dtype"],
                 dims=shape,
                 vals=raw_values))
-        input_proto = make_tensor_value_info(name, node.attr["dtype"],
-                                             shape)
+        input_proto = make_tensor_value_info(name, node.attr["dtype"], shape)
         inputs_proto.append(input_proto)
       else:
-        handler_name = "handle_" + op_name_to_lower(node.op)
+        splitted_op_name = node.op.split(".")
+        op_domain = "" if len(splitted_op_name) == 1 else ".".join(
+            splitted_op_name[:-1])
+        op_name = splitted_op_name[-1]
 
-        versions = frontend_tf_opset_version[op_name_to_lower(node.op)]
+        handler_name = "handle_" + op_name_to_lower(op_name)
 
-        opset_ver = opset[""]
-        assert isinstance(
-          opset_ver, int
-        ) and (opset_ver <= defs.onnx_opset_version()) and (
-            opset_ver >= 0
-        ), "Opset should be an int less than or equal to {}, but {}: {}".format(
-            defs.onnx_opset_version(), type(opset_ver), opset_ver)
+        # TODO per domain frontend_tf_opset_version?
+        versions = frontend_tf_opset_version[op_name_to_lower(op_name)]
 
+        opset_dict = {}
+        onnx_domain = defs.ONNX_DOMAIN
+        for domain, version in opset:
+          if domain == "ai.onnx":
+            domain = ""
+          opset_dict[domain] = version
+          defs.ONNX_DOMAIN = domain
+          assert isinstance(
+              version, int
+          ) and (version <= defs.onnx_opset_version()) and (
+              version >= 0
+          ), "Opset should be an int less than or equal to {}, but {}: {}".format(
+              defs.onnx_opset_version(), type(version), version)
+          defs.ONNX_DOMAIN = onnx_domain
+
+        opset_ver = opset_dict[op_domain]
         if opset_ver == 0:
           version = max(versions)
         else:
@@ -211,17 +224,18 @@ class TensorflowFrontendBase(object):
           version = versions[
               max([i for i, v in enumerate(versions) if v == opset_ver]) - 1]
 
+        camel_domain = "".join(w.title() for w in op_domain.split("."))
         frontend_ver = 'frontend_v{}'.format(version)
         frontend = cls.frontend_version_cache.setdefault(
-            frontend_ver,
-            importlib.import_module('onnx_tf.frontends.' + frontend_ver)
-            .TensorflowFrontend)
+            frontend_ver + "_" + op_domain,
+            getattr(
+                importlib.import_module('onnx_tf.frontends.' + frontend_ver),
+                "{}TensorflowFrontend".format(camel_domain)))
 
         # Check if specialized handler exists.
         if hasattr(frontend, handler_name):
           method_to_call = getattr(frontend, handler_name)
-          node = method_to_call(
-              node, consts=consts, node_dict=dict(node_tup))
+          node = method_to_call(node, consts=consts, node_dict=dict(node_tup))
           if isinstance(node, list):
             ops_proto.extend(node)
           else:
@@ -279,9 +293,9 @@ class TensorflowFrontendBase(object):
     :param graph_def: Tensorflow Graph Proto object.
     :param output: A string specifying the name of the output
       graph node.
-    :param opset: Opset version number or dict.
+    :param opset: Opset version number or list.
       Default is 0 means using latest version with domain ''.
-      Dict should be {str domain: int version number, }.
+      List item should be tuple (str domain, int version number).
     :param producer_name: The name of the producer.
     :param graph_name: The name of the output ONNX Graph.
 
@@ -296,13 +310,13 @@ class TensorflowFrontendBase(object):
           "Node {} is not found in the graph provided".format(name))
 
     assert isinstance(
-        opset, (int, dict)), "opset is expected to int or dict, but {}.".format(
-            type(opset))
+        opset,
+        (int, list,
+         tuple)), "opset is expected to int, list or tuple, but {}.".format(
+             type(opset))
     if isinstance(opset, int):
-      opset = {"": opset}
-    opset_imports = [
-        helper.make_opsetid(item[0], item[1]) for item in opset.items()
-    ]
+      opset = [("", opset)]
+    opset_imports = [helper.make_opsetid(item[0], item[1]) for item in opset]
 
     output_node = get_node_by_name(graph_def.node, output)
     onnx_graph = cls.tensorflow_graph_to_onnx_graph(graph_def, output_node,
