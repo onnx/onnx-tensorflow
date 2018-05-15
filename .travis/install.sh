25d47dd2a60d39e26ae073c27ce72ef2fff48537
Tian Jin <tian.jin1@ibm.com>
2018-05-15 18:46:07 -0400

    remove pb version

--- .travis/install.sh
+++ .travis/install.sh
@@ -3,14 +3,6 @@
 scripts_dir=$(dirname $(readlink -e "${BASH_SOURCE[0]}"))
 source "$scripts_dir/common"
 
-# install protobuf.
-pb_dir="$build_cache_dir/pb"
-mkdir -p $pb_dir
-wget -qO- "https://github.com/google/protobuf/releases/download/v$PB_VERSION/protobuf-$PB_VERSION.tar.gz" | tar -xvz -C "$pb_dir" --strip-components 1
-ccache -z
-cd "$pb_dir" && ./configure && make && make check && sudo make install && sudo ldconfig
-ccache -s
-
 # install onnx.
 onnx_dir="$workdir/onnx"
 mkdir -p $onnx_dir
