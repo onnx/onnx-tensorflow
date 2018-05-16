ac8511d4879bfa541a36ac15af0badc716afde6f
Tian Jin <tian.jin1@ibm.com>
2018-05-16 13:53:07 -0400

    revert pb to 2.6.1, 3.0.0 is still too slow for travis

--- .travis/install.sh
+++ .travis/install.sh
@@ -3,21 +3,19 @@
 scripts_dir=$(dirname $(readlink -e "${BASH_SOURCE[0]}"))
 source "$scripts_dir/common"
 
-# install protobuf.
+# Install protobuf.
 pb_dir="$build_cache_dir/pb"
 mkdir -p $pb_dir
-wget -qO- "https://github.com/google/protobuf/releases/download/v$PB_VERSION/protobuf-cpp-$PB_VERSION.tar.gz" | tar -xvz -C "$pb_dir" --strip-components 1
-wget -qO- "https://github.com/google/protobuf/releases/download/v$PB_VERSION/protobuf-python-$PB_VERSION.tar.gz" | tar -xvz -C "$pb_dir" --strip-components 1
+wget -qO- "https://github.com/google/protobuf/releases/download/v$PB_VERSION/protobuf-$PB_VERSION.tar.gz" | tar -xvz -C "$pb_dir" --strip-components 1
 ccache -z
 cd "$pb_dir" && ./configure && make && make check && sudo make install && sudo ldconfig
-cd "$pb_dir/python" && python setup.py build && python setup.py install
 ccache -s
 
-# install onnx.
+# Install onnx.
 onnx_dir="$workdir/onnx"
 mkdir -p $onnx_dir
 cd "$onnx_dir" && git clone --recursive git://github.com/onnx/onnx.git
-# checkout the version specified.
+# Checkout the version specified.
 cd onnx && git checkout $ONNX_BRANCH
-# install onnx.
+# Install onnx.
 pip install -e .
