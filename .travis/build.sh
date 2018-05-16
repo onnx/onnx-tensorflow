0c6ec13327c6a062a1baa987adab414e8282dba1
Tian Jin <tian.jin1@ibm.com>
2018-05-16 18:37:30 -0400

    run all tests regardless

--- .travis/build.sh
+++ .travis/build.sh
@@ -9,5 +9,12 @@ onnx_tf_dir="$PWD"
 pip install -e $onnx_tf_dir
 
 python --version
+
+# Make sure we run through all tests.
+set +e
+
 python -m unittest discover test/backend/
 python -m unittest discover test/frontend/
+
+# Back to exit on error.
+set -e
