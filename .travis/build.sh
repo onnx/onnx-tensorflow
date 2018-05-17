d3746cb711c7771ed61539906b6c85cedc5026da
Tian Jin <tian.jin1@ibm.com>
2018-05-16 20:22:03 -0400

    simplify test discovery

--- .travis/build.sh
+++ .travis/build.sh
@@ -10,11 +10,4 @@ pip install -e $onnx_tf_dir
 
 python --version
 
-# Make sure we run through all tests.
-set +e
-
-python -m unittest discover test/backend/
-python -m unittest discover test/frontend/
-
-# Back to exit on error.
-set -e
+python -m unittest discover test
