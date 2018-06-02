fix BroadcastMixin

onnx_tf/handlers/backend/conv_mixin.py
@@ -159,7 +159,7 @@ class ConvMixin(BroadcastMixin):
             output, perm=get_perm_from_formats(compute_format, storage_format))
     else:
       bias = input_dict[node.inputs[2]]
-      bias = BroadcastMixin.explicit_broadcast([x, bias], compute_c_idx)
+      bias = cls.explicit_broadcast([x, bias], compute_c_idx)
 
       if support_cuda:
         output = tf.concat(convolved, axis=1)

onnx_tf/handlers/backend/equal.py
@@ -9,11 +9,11 @@ from .control_flow_mixin import ComparisonMixin
 
 @onnx_op("Equal")
 @tf_func(tf.equal)
-class Equal(ComparisonMixin, BackendHandler):
+class Equal(BroadcastMixin, ComparisonMixin, BackendHandler):
 
   @classmethod
   def version_1(cls, node, **kwargs):
-    return BroadcastMixin.limited_broadcast(node, **kwargs)
+    return cls.limited_broadcast(node, **kwargs)
 
   @classmethod
   def version_7(cls, node, **kwargs):

onnx_tf/handlers/backend/or.py
@@ -9,11 +9,11 @@ from .broadcast_mixin import BroadcastMixin
 
 @onnx_op("Or")
 @tf_func(tf.logical_or)
-class Or(LogicalMixin, BackendHandler):
+class Or(BroadcastMixin, LogicalMixin, BackendHandler):
 
   @classmethod
   def version_1(cls, node, **kwargs):
-    return [BroadcastMixin.limited_broadcast(node, **kwargs)]
+    return cls.limited_broadcast(node, **kwargs)
 
   @classmethod
   def version_7(cls, node, **kwargs):

onnx_tf/handlers/backend/pow.py
@@ -9,11 +9,11 @@ from .math_mixin import BasicMathMixin
 
 @onnx_op("Pow")
 @tf_func(tf.pow)
-class Pow(BasicMathMixin, BackendHandler):
+class Pow(BroadcastMixin, BasicMathMixin, BackendHandler):
 
   @classmethod
   def version_1(cls, node, **kwargs):
-    return BroadcastMixin.limited_broadcast(node, **kwargs)
+    return cls.limited_broadcast(node, **kwargs)
 
   @classmethod
   def version_7(cls, node, **kwargs):

onnx_tf/handlers/backend/sub.py
@@ -9,15 +9,15 @@ from .math_mixin import ArithmeticMixin
 
 @onnx_op("Sub")
 @tf_func(tf.subtract)
-class Sub(ArithmeticMixin, BackendHandler):
+class Sub(BroadcastMixin, ArithmeticMixin, BackendHandler):
 
   @classmethod
   def version_1(cls, node, **kwargs):
-    return BroadcastMixin.limited_broadcast(node, **kwargs)
+    return cls.limited_broadcast(node, **kwargs)
 
   @classmethod
   def version_6(cls, node, **kwargs):
-    return BroadcastMixin.limited_broadcast(node, **kwargs)
+    return cls.limited_broadcast(node, **kwargs)
 
   @classmethod
   def version_7(cls, node, **kwargs):

onnx_tf/handlers/backend/xor.py
@@ -9,11 +9,11 @@ from .broadcast_mixin import BroadcastMixin
 
 @onnx_op("Xor")
 @tf_func(tf.logical_xor)
-class Xor(LogicalMixin, BackendHandler):
+class Xor(BroadcastMixin, LogicalMixin, BackendHandler):
 
   @classmethod
   def version_1(cls, node, **kwargs):
-    return [BroadcastMixin.limited_broadcast(node, **kwargs)]
+    return cls.limited_broadcast(node, **kwargs)
 
   @classmethod
   def version_7(cls, node, **kwargs):