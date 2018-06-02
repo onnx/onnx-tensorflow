fix hardmax, log_softmax, softmax

onnx_tf/handlers/backend/hardmax.py
@@ -13,15 +13,15 @@ class Hardmax(BackendHandler):
   @classmethod
   def version_1(cls, node, **kwargs):
     x = kwargs["tensor_dict"][node.inputs[0]]
-    shape = x.get_shape().as_list()
     axis = node.attrs.get("axis", 1)
-    axis = axis if axis >= 0 else len(shape) + axis
+    axis = axis if axis >= 0 else len(np.shape(x)) + axis
 
-    if axis == len(shape) - 1:
+    if axis == len(np.shape(x)) - 1:
       return [cls.make_tensor_from_onnx_node(node, **kwargs)]
 
-    cal_shape = (np.prod(shape[0:axis], dtype=np.int64),
-                 np.prod(shape[axis:], dtype=np.int64))
+    shape = tf.shape(x)
+    cal_shape = (tf.reduce_prod(shape[0:axis]),
+                 tf.reduce_prod(shape[axis:tf.size(shape)]))
     x = tf.reshape(x, cal_shape)
 
     return [tf.reshape(tf.contrib.seq2seq.hardmax(x), shape)]