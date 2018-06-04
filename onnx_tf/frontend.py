9cd2cbb9b6beec74f4442f54df99f56ebd9f9b61
Wenhao Hu <fumihwh@gmail.com>
2018-06-04 11:50:48 +0900

    usr defs.ONNX_DOMAIN instead

--- onnx_tf/frontend.py
+++ onnx_tf/frontend.py
@@ -274,7 +274,8 @@ class TensorflowFrontendBase(object):
   def tensorflow_graph_to_onnx_graph(cls,
                                      graph_def,
                                      output,
-                                     opset=(("", 0),),
+                                     opset=((defs.ONNX_DOMAIN,
+                                             defs.onnx_opset_version()),),
                                      name="graph",
                                      ignore_unimplemented=False):
     """Converts a Tensorflow Graph Proto to an ONNX graph
@@ -300,7 +301,7 @@ class TensorflowFrontendBase(object):
     opset_dict = {}
     for domain, version in opset:
       if domain == "ai.onnx":
-        domain = ""
+        domain = defs.ONNX_DOMAIN
       opset_dict[domain] = version
 
     handlers = get_all_frontend_handlers(opset_dict)
@@ -326,8 +327,7 @@ class TensorflowFrontendBase(object):
               data_type_cast_map=onnx_graph.data_type_cast_map)
         else:
           exception.OP_UNIMPLEMENTED_EXCEPT(
-              node.op,
-              domain=None if node.domain in handlers else node.domain)
+              node.op, domain=None if node.domain in handlers else node.domain)
         if node_proto is None:
           node_proto = FrontendHandler.make_node_from_tf_node(
               node, op_type=node.op, should_check=False)
@@ -386,7 +386,7 @@ class TensorflowFrontendBase(object):
       raise TypeError("opset is expected to int, list or tuple, but {}.".format(
           type(opset)))
     if isinstance(opset, (int, long)):
-      opset = [("", opset or defs.onnx_opset_version())]
+      opset = [(defs.ONNX_DOMAIN, opset or defs.onnx_opset_version())]
     opset_imports = [make_opsetid(item[0], item[1]) for item in opset]
 
     output_node = get_node_by_name(graph_def.node, output)
