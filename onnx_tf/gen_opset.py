56909578be55e6e52abc2f805529266a0cd0fe50
Wenhao Hu <fumihwh@gmail.com>
2018-06-06 00:20:28 +0900

    usr defs.ONNX_DOMAIN instead (#206)

--- onnx_tf/gen_opset.py
+++ onnx_tf/gen_opset.py
@@ -42,7 +42,7 @@ def main():
     version += 1
 
   frontend_onnx_coverage, frontend_tf_coverage = get_frontend_coverage()
-  frontend_opset_dict.update(frontend_onnx_coverage.get("", {}))
+  frontend_opset_dict.update(frontend_onnx_coverage.get(defs.ONNX_DOMAIN, {}))
 
   with open('opset_version.py', 'w') as version_file:
     pp = pprint.PrettyPrinter(indent=4)
@@ -51,7 +51,7 @@ def main():
     version_file.write("frontend_opset_version = {\n " +
                        pp.pformat(frontend_opset_dict)[1:-1] + "\n}\n\n")
     version_file.write("frontend_tf_opset_version = {\n " + pp.pformat(
-        frontend_tf_coverage.get("", {}))[1:-1] + "\n}\n")
+        frontend_tf_coverage.get(defs.ONNX_DOMAIN, {}))[1:-1] + "\n}\n")
 
 
 if __name__ == '__main__':
