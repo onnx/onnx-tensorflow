5b48194415d73ad74bb24fc0be2bf84a5dea9db6
Tian Jin <tjingrant@gmail.com>
2018-06-10 12:29:25 -0400

    some legacy fix

--- test/backend/test_onnx_backend.py
+++ test/backend/test_onnx_backend.py
@@ -23,14 +23,18 @@ backend_test.exclude(r'[a-z,_]*GLU[a-z,_]*')
 backend_test.exclude(r'[a-z,_]*dilated_strided[a-z,_]*')
 backend_test.exclude(r'[a-z,_]*Conv2d_dilated[a-z,_]*')
 
-# PRelu OnnxBackendPyTorchConvertedModelTest has wrong dim for broadcasting
-backend_test.exclude(r'[a-z,_]*PReLU_[0-9]d_multiparam[a-z,_]*')
-
-# Skip experimental Upsample
+# Experimental op we do not currently support:
 backend_test.exclude(r'[a-z,_]*Upsample[a-z,_]*')
 
 if 'TRAVIS' in os.environ:
   backend_test.exclude('test_vgg19')
+
+major, minor, revision = map(int, onnx.version.version.split("."))
+if major == 1 and minor < 2:
+	backend_test.exclude('test_operator_add_broadcast_cpu')
+	backend_test.exclude('test_operator_add_size1_broadcast_cpu')
+	backend_test.exclude('test_operator_add_size1_right_broadcast_cpu')
+	backend_test.exclude('test_operator_add_size1_singleton_broadcast_cpu')
 
 # import all test cases at global scope to make them visible to python.unittest
 globals().update(backend_test.enable_report().test_cases)
