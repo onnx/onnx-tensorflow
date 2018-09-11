6d001833634240d626a335a881483046fbcea1d2
Tian Jin <tjingrant@gmail.com>
2018-09-11 16:23:21 -0400

    Simplify logic.

--- onnx_tf/common/data_type.py
+++ onnx_tf/common/data_type.py
@@ -7,26 +7,22 @@ import tensorflow as tf
 
 
 def tf2onnx(dtype):
+  if isinstance(dtype, Number):
+    tf_dype = tf.as_dtype(dtype)
+  elif isinstance(dtype, tf.DType):
+    tf_dype = dtype
+  else:
+    raise RuntimeError("dtype should be number or tf.DType.")
+    
   # Usually, tf2onnx is done via tf_type->numpy_type->onnx_type
   # to leverage existing type conversion infrastructure;
   # However, we need to intercept the string type early because 
   # lowering tf.string type to numpy dtype results in loss of 
   # information. <class 'object'> is returned instead of the 
   # numpy string type desired.
-  if isinstance(dtype, Number):
-    # Check if dtype is DT_STRING as defined
-    # https://github.com/tensorflow/tensorflow/blob/36d7b12357df667dcd427c070e21779ed83f4ec9/tensorflow/core/framework/types.proto#L24
-    if dtype is 7:
-      return TensorProto.String
-    else:
-      tf_dype = tf.as_dtype(dtype)
-  elif isinstance(dtype, tf.DType):
-    if tf_dype is tf.string:
-      return TensorProto.STRING
-    else:
-      tf_dype = dtype
-  else:
-    raise RuntimeError("dtype should be number or tf.DType.")
+  if tf_dype is tf.string:
+    return TensorProto.STRING
+  
   return mapping.NP_TYPE_TO_TENSOR_TYPE[np.dtype(tf_dype.as_numpy_dtype)]
 
 
