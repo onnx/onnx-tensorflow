0fa994b09ebcdb079817cef2c0d0ca9cf30dbff5
Tian Jin <tjingrant@gmail.com>
2018-09-11 13:55:48 -0400

    Update data_type.py

--- onnx_tf/common/data_type.py
+++ onnx_tf/common/data_type.py
@@ -7,15 +7,20 @@ import tensorflow as tf
 
 
 def tf2onnx(dtype):
+  # Usually, tf2onnx is done via tf_type->numpy_type->onnx_type
+  # to leverage existing type conversion infrastructure;
+  # However, we need to intercept the string type early because 
+  # lowering tf.string type to numpy dtype results in loss of 
+  # information. <class 'object'> is returned instead of the 
+  # numpy string type desired.
   if isinstance(dtype, Number):
-    tf_dype = tf.as_dtype(dtype)
+    # Check if dtype is DT_STRING as defined
+    # https://github.com/tensorflow/tensorflow/blob/36d7b12357df667dcd427c070e21779ed83f4ec9/tensorflow/core/framework/types.proto#L24
+    if tf_dype is 7:
+      return TensorProto.String
+    else:
+      tf_dype = tf.as_dtype(dtype)
   elif isinstance(dtype, tf.DType):
-    # Usually, tf2onnx is done via tf_type->numpy_type->onnx_type
-    # to leverage existing type conversion infrastructure;
-    # However, we need to intercept the string type early because 
-    # lowering tf.string type to numpy dtype results in loss of 
-    # information. <class 'object'> is returned instead of the 
-    # numpy string type desired.
     if tf_dype is tf.string:
       return TensorProto.STRING
     else:
