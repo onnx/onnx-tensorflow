c1462854f196b1be72512bb6f150ad2b61f7322b
Wenhao Hu <fumihwh@gmail.com>
2018-10-02 10:58:11 +0900

    remove unnecessary check and refactor

--- onnx_tf/handlers/frontend/split.py
+++ onnx_tf/handlers/frontend/split.py
@@ -11,9 +11,6 @@ class Split(FrontendHandler):
   @classmethod
   def args_check(cls, node, **kwargs):
     if node.op_type == "SplitV":
-      if cls.SINCE_VERSION == 2:
-        if node.inputs[1] not in kwargs["consts"]:
-          exception.CONST_NOT_FOUND_EXCEPT(node.inputs[1], node.op_type)
       if node.inputs[2] not in kwargs["consts"]:
         exception.CONST_NOT_FOUND_EXCEPT(node.inputs[2], node.op_type)
     if node.op_type == "Split":
@@ -25,7 +22,7 @@ class Split(FrontendHandler):
     consts = kwargs["consts"]
     if node.op_type == "SplitV":
       axis = int(consts[node.inputs[2]])
-      x = [node.inputs[0], node.inputs[1]]
+      x = [node.inputs[0], node.attr["num_split"]]
     elif node.op_type == "Split":
       axis = int(consts[node.inputs[0]])
       x = [node.inputs[1], node.attr["num_split"]]
@@ -40,15 +37,13 @@ class Split(FrontendHandler):
     consts = kwargs["consts"]
     if node.op_type == "SplitV":
       axis = int(consts[node.inputs[2]])
-      split = consts[node.inputs[1]]
       x = [node.inputs[0]]
     elif node.op_type == "Split":
       axis = int(consts[node.inputs[0]])
-      split = [shape[axis] for shape in node.attr["_output_shapes"]]
       x = [node.inputs[1]]
     return cls.make_node_from_tf_node(
         node,
         x,
         cls.get_outputs_names(node, num=node.attr["num_split"]),
-        split=split,
+        split=[shape[axis] for shape in node.attr["_output_shapes"]],
         axis=axis)
