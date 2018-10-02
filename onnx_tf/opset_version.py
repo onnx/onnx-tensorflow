68935b5f47deccb27c0335914dd1eb680637f755
Wenhao Hu <fumihwh@gmail.com>
2018-10-02 22:02:39 +0900

    Add Split (#265)
    * Add Split
    
    * remove unnecessary check and refactor

--- onnx_tf/opset_version.py
+++ onnx_tf/opset_version.py
@@ -303,6 +303,7 @@ frontend_tf_opset_version = {
     'softplus': [1],
     'softsign': [1],
     'space_to_depth': [1],
+    'split': [1, 2],
     'split_v': [1, 2],
     'sqrt': [1, 6],
     'squeeze': [1],
