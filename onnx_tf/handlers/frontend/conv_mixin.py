f7351237ef5ce54ff5c9d3e24ec2093d43510986
Wenhao Hu <fumihwh@gmail.com>
2018-10-15 23:31:11 +0900

    add transpose nodes to conv if NHWC

--- onnx_tf/handlers/frontend/conv_mixin.py
+++ onnx_tf/handlers/frontend/conv_mixin.py
@@ -1,3 +1,4 @@
+from onnx_tf.common import get_perm_from_formats
 from onnx_tf.common import get_unique_suffix
 
 
@@ -27,21 +28,53 @@ class ConvMixin(object):
             spatial_indices))
     pads = cls.cal_pads(auto_pad, len(spatial_indices), input_shape,
                         output_shape, strides, kernel_shape)
-    unique_suffix = get_unique_suffix()
-    transpose_node = cls.make_node_from_tf_node(
-        node, [node.inputs[1]], [node.inputs[1] + "_T_" + unique_suffix],
+
+    w_unique_suffix = get_unique_suffix()
+    w_transpose_node = cls.make_node_from_tf_node(
+        node, [node.inputs[1]], [node.inputs[1] + "_T_" + w_unique_suffix],
         perm=[d + 1, d] + list(range(d)),
         op_type="Transpose",
-        name=node.inputs[1] + "_T_" + unique_suffix)
-    conv_node = cls.make_node_from_tf_node(
-        node, [node.inputs[0], node.inputs[1] + "_T_" + unique_suffix],
-        pads=pads,
-        group=n_groups,
-        kernel_shape=kernel_shape,
-        strides=strides,
-        dilations=dilations)
+        name=node.inputs[1] + "_T_" + w_unique_suffix)
 
-    return [transpose_node, conv_node]
+    if data_format[-1] == "C":
+      c_first_data_format = data_format[0] + "C" + data_format[1:-1]
+      pre_unique_suffix = get_unique_suffix()
+      pre_transpose_node = cls.make_node_from_tf_node(
+          node, [node.inputs[0]], [node.inputs[0] + "_T_" + pre_unique_suffix],
+          perm=get_perm_from_formats(data_format, c_first_data_format),
+          op_type="Transpose",
+          name=node.inputs[0] + "_T_" + pre_unique_suffix)
+
+      conv_unique_suffix = get_unique_suffix()
+      conv_output = cls.get_outputs_names(node)[0]
+      conv_node = cls.make_node_from_tf_node(
+          node, [pre_transpose_node.output[0], w_transpose_node.output[0]],
+          [conv_output + "_C_" + conv_unique_suffix],
+          pads=pads,
+          group=n_groups,
+          kernel_shape=kernel_shape,
+          strides=strides,
+          dilations=dilations)
+
+      post_unique_suffix = get_unique_suffix()
+      post_transpose_node = cls.make_node_from_tf_node(
+          node, [conv_node.output[0]], [conv_output],
+          perm=get_perm_from_formats(c_first_data_format, data_format),
+          op_type="Transpose",
+          name=conv_output + "_C_" + conv_unique_suffix + "_T_" +
+          post_unique_suffix)
+      return [
+          pre_transpose_node, w_transpose_node, conv_node, post_transpose_node
+      ]
+    else:
+      conv_node = cls.make_node_from_tf_node(
+          node, [node.inputs[0], w_transpose_node.output[0]],
+          pads=pads,
+          group=n_groups,
+          kernel_shape=kernel_shape,
+          strides=strides,
+          dilations=dilations)
+      return [w_transpose_node, conv_node]
 
   @staticmethod
   def cal_pads(auto_pad, spatial_dim, input_shape, output_shape, strides,
