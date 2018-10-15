a378d80e637fa047cfd669b74af32dc31ff67230
Wenhao Hu <fumihwh@gmail.com>
2018-10-15 14:06:40 +0900

    add model converter API (#274)
    * add model converter API
    
    *  refactor and add console_scripts
    
    * edit readme
    
    * add cli
    
    * edit README.md
    
    * refactor README.md
    
    * edit README.md
    
    * add converting from ckpt
    
    * use underscore
    
    * add cli test
    
    * rename var
    
    * bug fix
    
    * make prepare_model as staticmethod
    
    * refactor
    
    * move get_output_node_names to common
    
    * nit
    
    * use key arg
    
    * support non staticmethod
    
    * nit
    
    * revert key arg
    
    * add debug code
    
    * remove debug code
    
    * skip test if onnx < 1.2.1
    
    * bug fix
    
    * add doc
    
    * remove --ignore_unimplemented True and add comments
    
    * comments

---
+++ onnx_tf/cli.py
@@ -0,0 +1,21 @@
+import argparse
+import sys
+
+import onnx_tf.converter
+
+
+def main():
+  args = sys.argv[1:]
+  parser = argparse.ArgumentParser(
+      description="ONNX-Tensorflow Command Line Interface")
+  parser.add_argument(
+      "command", choices=["convert"], help="Available commands.")
+  if len(args) == 0:
+    parser.parse_args(["-h"])
+  cli_tool = parser.parse_args([args[0]])
+  if cli_tool.command == "convert":
+    return onnx_tf.converter.main(args[1:])
+
+
+if __name__ == '__main__':
+  main()
