ff5ad1f4f5bddb03145caeb0c8d1a268867bb004
Wenhao Hu <fumihwh@gmail.com>
2018-10-18 22:26:03 +0900

    return undefined dtype when failed at converting dtype from tf to onnx

--- onnx_tf/common/data_type.py
+++ onnx_tf/common/data_type.py
@@ -1,4 +1,5 @@
 from numbers import Number
+import warnings
 
 import numpy as np
 from onnx import mapping
@@ -15,17 +16,27 @@ def tf2onnx(dtype):
     return [tf2onnx(t) for t in dtype]
   else:
     raise RuntimeError("dtype should be number or tf.DType.")
-    
+
   # Usually, tf2onnx is done via tf_type->numpy_type->onnx_type
   # to leverage existing type conversion infrastructure;
-  # However, we need to intercept the string type early because 
-  # lowering tf.string type to numpy dtype results in loss of 
-  # information. <class 'object'> is returned instead of the 
+  # However, we need to intercept the string type early because
+  # lowering tf.string type to numpy dtype results in loss of
+  # information. <class 'object'> is returned instead of the
   # numpy string type desired.
   if tf_dype is tf.string:
     return TensorProto.STRING
-  
-  return mapping.NP_TYPE_TO_TENSOR_TYPE[np.dtype(tf_dype.as_numpy_dtype)]
+
+  onnx_dtype = None
+  try:
+    onnx_dtype = mapping.NP_TYPE_TO_TENSOR_TYPE[np.dtype(
+        tf_dype.as_numpy_dtype)]
+  finally:
+    if onnx_dtype is None:
+      warnings.warn(
+          "Can't convert tf dtype {} to ONNX dtype. Return 0 (TensorProto.UNDEFINED).".
+          format(tf_dype))
+      onnx_dtype = TensorProto.UNDEFINED
+    return onnx_dtype
 
 
 def onnx2tf(dtype):
