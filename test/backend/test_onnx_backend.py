cf5848d12eb2d1b7809268cc8125d912fd8ffa0a
Tian Jin <tjingrant@gmail.com>
2018-11-12 12:22:08 -0500

    exclude compress

--- test/backend/test_onnx_backend.py
+++ test/backend/test_onnx_backend.py
@@ -9,7 +9,7 @@ import unittest
 import onnx.backend.test
 
 from onnx_tf.backend import TensorflowBackend
-from onnx_tf.common.legacy import legacy_onnx_pre_ver
+from onnx_tf.common.legacy import legacy_onnx_pre_ver, legacy_opset_pre_ver
 
 # This is a pytest magic variable to load extra plugins
 pytest_plugins = 'onnx.backend.test.report',
@@ -50,6 +50,9 @@ if legacy_onnx_pre_ver(1, 2):
   backend_test.exclude(r'test_operator_rnn_cpu')
   backend_test.exclude(r'test_operator_lstm_cpu')
   backend_test.exclude(r'test_operator_rnn_single_layer_cpu')
+
+if legacy_opset_pre_ver(9):
+  backend_test.exclude(r'test_compress')
 
 # import all test cases at global scope to make them visible to python.unittest
 globals().update(backend_test.enable_report().test_cases)
