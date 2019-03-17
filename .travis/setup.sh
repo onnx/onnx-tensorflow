cf39aa378689433df8fdc8287eef3f2079415f56
Tian Jin <tjingrant@gmail.com>
2019-03-17 00:34:10 -0400

    use scripts to determine release/normal build, and use persistent container

--- .travis/setup.sh
+++ .travis/setup.sh
@@ -1,1 +1,16 @@
 export TRAVIS=1
+
+# Check if head has a tag?
+git describe --exact-match --tags HEAD
+if [ $? -eq 0 ]; then
+	echo "This is a release test for onnx-tf."
+	export DOCKER_CONTAINER_NAME="$(git describe --exact-match --tags HEAD)"
+else
+	echo "This is a non-release test for onnx-tf."
+	# TODO: switch to a docker file that always pulls the tip of onnx and tf.
+	export DOCKER_CONTAINER_NAME="onnx1.4.1-tf1.13.1"
+fi
+
+echo "Docker container is determined to be ${DOCKER_CONTAINER_NAME}."
+
+docker pull winnietsang/onnx-tensoflow:${DOCKER_CONTAINER_NAME}
