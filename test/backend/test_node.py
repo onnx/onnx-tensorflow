d21b7c6c8436a57fb444b267e37dd2fe4b7de854
Simeon Monov <sdmonov@us.ibm.com>
2019-07-19 21:17:11 +0200

    Removed unused import in test_node.py

--- test/backend/test_node.py
+++ test/backend/test_node.py
@@ -8,7 +8,6 @@ import unittest
 import numpy as np
 import tensorflow as tf
 from onnx_tf.backend import run_node
-from onnx_tf.backend import prepare
 from onnx_tf.common import supports_device
 from onnx_tf.common.legacy import legacy_onnx_pre_ver, legacy_opset_pre_ver
 from onnx import helper
