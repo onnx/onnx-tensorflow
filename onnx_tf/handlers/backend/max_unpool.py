42675b71749b7874a1fc893ea797466f0121e72d
Simeon Monov <sdmonov@us.ibm.com>
2019-11-12 15:48:37 -0800

    Max unpool tests moved to test_node and added max_unpool v11
    * Max unpool tests moved from test_module.py to test_node.py
    * Added v11 for MaxUnpool op
    * Fixed formatting

--- onnx_tf/handlers/backend/max_unpool.py
+++ onnx_tf/handlers/backend/max_unpool.py
@@ -6,6 +6,10 @@ from .unpool_mixin import UnpoolMixin
 @onnx_op("MaxUnpool")
 class MaxUnpool(UnpoolMixin, BackendHandler):
 
-  @classmethod
-  def version_9(cls, node, **kwargs):
-    return cls.max_unpool(node, kwargs["tensor_dict"])
+    @classmethod
+    def version_9(cls, node, **kwargs):
+        return cls.max_unpool(node, kwargs["tensor_dict"])
+
+    @classmethod
+    def version_11(cls, node, **kwargs):
+        return cls.max_unpool(node, kwargs["tensor_dict"])
