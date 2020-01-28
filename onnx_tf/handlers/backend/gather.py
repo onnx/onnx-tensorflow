28613f45a9d1fee745debef2b73fecfbb3e34fb8
winstonq <winstonq@hotpads.com>
2020-01-27 16:47:23 -0800

    use mixin for gather op validation

--- onnx_tf/handlers/backend/gather.py
+++ onnx_tf/handlers/backend/gather.py
@@ -3,11 +3,12 @@ import tensorflow as tf
 from onnx_tf.handlers.backend_handler import BackendHandler
 from onnx_tf.handlers.handler import onnx_op
 from onnx_tf.handlers.handler import tf_func
+from .gather_and_scatter_mixin import GatherAndScatterMixin
 
 
 @onnx_op("Gather")
 @tf_func(tf.gather)
-class Gather(BackendHandler):
+class Gather(GatherAndScatterMixin, BackendHandler):
 
   @classmethod
   def version_1(cls, node, **kwargs):
@@ -18,16 +19,8 @@ class Gather(BackendHandler):
     x = kwargs["tensor_dict"][node.inputs[0]]
     indices = kwargs["tensor_dict"][node.inputs[1]]
     axis = node.attrs.get("axis", 0)
-    dimension = tf.cast(tf.shape(x)[axis], indices.dtype)
-    indices_shape = tf.shape(indices)
-    dim_broadcasted = tf.broadcast_to(dimension, indices_shape)
-    nonneg_indices = tf.cast(tf.greater_equal(indices, 0), dtype=indices.dtype)
-    negative_indices = tf.cast(tf.less(indices, 0), dtype=indices.dtype)
-    final_indices = dim_broadcasted * negative_indices + negative_indices * indices + nonneg_indices * indices
-    cond1 = tf.greater_equal(final_indices, 0)
-    cond2 = tf.less(final_indices, dimension)
-    indice_valiation = tf.reduce_all(tf.logical_and(cond1, cond2))
+    result = cls.chk_idx_out_of_bounds_along_axis(x, axis, indices)
     msg = 'Gather indices are out of bounds, please double check the indices and retry.'
-    with tf.control_dependencies(
-        [tf.compat.v1.assert_equal(indice_valiation, True, message=msg)]):
-      return [tf.gather(x, final_indices, axis=axis, name=cls.tf_node_name(node.name))]
+    with tf.control_dependencies([tf.compat.v1.assert_equal(result, True, message=msg)]):
+      indices = cls.process_neg_idx_along_axis(x, axis, indices)
+      return [tf.gather(x, indices, axis=axis, name=cls.tf_node_name(node.name))]
