bf80c7e95c4e40d24cecf6e715016aa3dd4ca9d8
Shahir Daya <sdaya@ca.ibm.com>
2020-05-28 18:16:25 -0400

    Add Opset 12 support for ArgMin (#623)

--- onnx_tf/handlers/backend/arg_min.py
+++ onnx_tf/handlers/backend/arg_min.py
@@ -3,6 +3,7 @@ import tensorflow as tf
 from onnx_tf.handlers.backend_handler import BackendHandler
 from onnx_tf.handlers.handler import onnx_op
 from onnx_tf.handlers.handler import tf_func
+from onnx_tf.common.tf_helper import tf_shape
 
 
 @onnx_op("ArgMin")
@@ -17,10 +18,19 @@ class ArgMin(BackendHandler):
   def _common(cls, node, **kwargs):
     axis = node.attrs.get("axis", 0)
     keepdims = node.attrs.get("keepdims", 1)
-    arg_max = cls.make_tensor_from_onnx_node(node, **kwargs)
+    select_last_index = node.attrs.get("select_last_index", 0)
+    if select_last_index == 0:
+      arg_min = cls.make_tensor_from_onnx_node(node, **kwargs)
+    else:
+      # reverse the input and apply argmax on that to get last occurrence of max
+      x = kwargs["tensor_dict"][node.inputs[0]]
+      x = tf.reverse(x, axis=[axis])
+      arg_min = cls.make_tensor_from_onnx_node(node, inputs=[x], **kwargs)
+      # adjust indices to account for the reverse
+      arg_min = tf_shape(x)[axis] - arg_min - 1
     if keepdims == 1:
-      return [tf.expand_dims(arg_max, axis=axis)]
-    return [arg_max]
+      return [tf.expand_dims(arg_min, axis=axis)]
+    return [arg_min]
 
   @classmethod
   def version_1(cls, node, **kwargs):
@@ -28,4 +38,8 @@ class ArgMin(BackendHandler):
 
   @classmethod
   def version_11(cls, node, **kwargs):
+    return cls._common(node, **kwargs)
+
+  @classmethod
+  def version_12(cls, node, **kwargs):
     return cls._common(node, **kwargs)
