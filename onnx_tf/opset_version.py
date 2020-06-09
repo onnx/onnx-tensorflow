0770fa9a1b8948ef90abb6ccd121a5e02221527a
Winnie Tsang <wtsang@us.ibm.com>
2020-06-09 23:40:00 +0000

    Fix Clip optional inputs handle and add Opset 12 & 13 support

--- onnx_tf/opset_version.py
+++ onnx_tf/opset_version.py
@@ -22,7 +22,7 @@ backend_opset_version = {
     'CategoryMapper': [],
     'Ceil': [1, 6],
     'Celu': [],
-    'Clip': [1, 6, 11],
+    'Clip': [1, 6, 11, 12, 13],
     'Compress': [9, 11],
     'Concat': [1, 4, 11],
     'ConcatFromSequence': [],
