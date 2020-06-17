92f7eac75c1d8c7436e23118fa9762f6c7aa2241
Jason Plurad <pluradj@us.ibm.com>
2020-06-17 10:48:31 -0400

    Fix pooling string comparison and case for auto_pad
    Signed-off-by: Jason Plurad <pluradj@us.ibm.com>

--- onnx_tf/common/pooling_helper.py
+++ onnx_tf/common/pooling_helper.py
@@ -173,6 +173,9 @@ def py_pool(input, kernel_shape, strides=None, dilations=None,
         padding = [0] * spatial_size * 2
 
     if type(padding) is not list and type(padding) is not np.ndarray:
+        if type(padding) is not str:
+            padding = padding.decode("UTF-8")
+
         if padding.lower().startswith("same"):
             padding = calc_pads_same(inp_sp_shape, kernel_shape, strides,
                                      dilations, padding)
