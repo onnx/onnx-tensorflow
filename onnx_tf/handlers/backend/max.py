e827bb26f7854856e00054d127ebbc79fb24d14a
Winnie Tsang <wtsang@us.ibm.com>
2020-09-05 06:28:50 +0000

    Fix Loop and If handlers
    1. Fix Loop and If handlers to run the new onnx_backend_test
    2. Add initializers from all the subgraphs into tensor_dict
    3. Update If, Loop and Scan to make a copy of tensor_dict and
    add in all the inputs and outputs of the subgraph into this
    tensor_dict copy and then send it to
    backend.onnx_graph_to_tensorflow_ops
    4. Update backend.onnx_graph_to_tensorflow_ops to eliminate tensor_dict
    and just use the subgraph_tensor_dict from IF, Loop and Scan
    5. Fix Min and Max to support inputs that are in different shapes
    6. Add dynamic input support to SequenceInsert
    7. Add Opset 13 support for If, Loop, Concat, Constant, Matmul, Sub
    8. Fix issue #742
    
    Signed-off-by: Winnie Tsang <wtsang@us.ibm.com>

--- onnx_tf/handlers/backend/max.py
+++ onnx_tf/handlers/backend/max.py
@@ -1,25 +1,45 @@
 import tensorflow as tf
 
+from onnx_tf.common import exception
+from onnx_tf.common import data_type
+from onnx_tf.common import sys_config
 from onnx_tf.handlers.backend_handler import BackendHandler
 from onnx_tf.handlers.handler import onnx_op
-from onnx_tf.handlers.handler import tf_func
 
 
 @onnx_op("Max")
-@tf_func(tf.reduce_max)
 class Max(BackendHandler):
+  supported_types = [
+      tf.bfloat16, tf.float16, tf.float32, tf.float64, tf.int32, tf.int64
+  ]
+  cast_map = {
+      tf.uint8: tf.int32,
+      tf.uint16: tf.int32,
+      tf.uint32: tf.int64,
+      tf.int8: tf.int32,
+      tf.int16: tf.int32
+  }
+  cast_map[tf.uint64] = tf.int64 if sys_config.auto_cast else None
 
   @classmethod
-  def get_attrs_processor_param(cls):
-    return {"default": {"axis": 0}}
+  def args_check(cls, node, **kwargs):
+    dtype = kwargs["tensor_dict"][node.inputs[0]].dtype
+    if dtype in cls.cast_map and cls.cast_map[dtype] is None:
+      exception.DTYPE_NOT_CAST_EXCEPT(
+          "Max input " + node.inputs[0] + " with data type '" +
+          data_type.tf_to_np_str(dtype) + "'",
+          data_type.tf_to_np_str_list(cls.supported_types))
 
   @classmethod
   def _common(cls, node, **kwargs):
     values = [kwargs["tensor_dict"][inp] for inp in node.inputs]
-    return [
-        cls.make_tensor_from_onnx_node(
-            node, inputs=[tf.stack(values)], **kwargs)
-    ]
+    dtype = values[0].dtype
+    if dtype in cls.cast_map:
+      values = [tf.cast(v, cls.cast_map[v.dtype]) for v in values]
+    result = values[0]
+    for i in range(1, len(values)):
+      result = tf.maximum(result, values[i])
+    return [tf.cast(result, dtype) if dtype in cls.cast_map else result]
 
   @classmethod
   def version_1(cls, node, **kwargs):
