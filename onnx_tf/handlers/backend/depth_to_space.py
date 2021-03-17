6685f45f3fed37a7d8868c4ecabe8390e489a67c
Dom Miketa <dominik.miketa@gmail.com>
2021-03-17 00:02:27 +0000

    fix dynamic shape inference in DepthToSpace (#880)
    * fix dynamic shape inference in DepthToSpace
    
    Signed-off-by: Dom Miketa <dominik.miketa@gmail.com>
    
    * add unittest
    
    Signed-off-by: Dom Miketa <dominik.miketa@gmail.com>
    
    * formatting with yapf and pylint
    
    Signed-off-by: Dom Miketa <dominik.miketa@gmail.com>
    
    * format unittest
    
    Signed-off-by: Dom Miketa <dominik.miketa@gmail.com>

--- onnx_tf/handlers/backend/depth_to_space.py
+++ onnx_tf/handlers/backend/depth_to_space.py
@@ -3,6 +3,7 @@ import copy
 import tensorflow as tf
 
 from onnx_tf.common import get_data_format
+from onnx_tf.common.tf_helper import tf_shape
 from onnx_tf.handlers.backend_handler import BackendHandler
 from onnx_tf.handlers.handler import onnx_op
 from onnx_tf.handlers.handler import tf_func
@@ -24,15 +25,17 @@ class DepthToSpace(BackendHandler):
     attrs = copy.deepcopy(node.attrs)
     attrs["data_format"] = storage_format
     return [
-        cls.make_tensor_from_onnx_node(
-            node, attrs=attrs, c_first_cuda_only=True, **kwargs)
+        cls.make_tensor_from_onnx_node(node,
+                                       attrs=attrs,
+                                       c_first_cuda_only=True,
+                                       **kwargs)
     ]
 
   @classmethod
   def _common(cls, node, **kwargs):
     x = kwargs["tensor_dict"][node.inputs[0]]
     x_rank = len(x.get_shape())
-    storage_format, compute_format = get_data_format(x_rank)
+    storage_format, _ = get_data_format(x_rank)
     attrs = copy.deepcopy(node.attrs)
     attrs["data_format"] = storage_format
     mode = attrs.get("mode", "DCR")
@@ -40,7 +43,9 @@ class DepthToSpace(BackendHandler):
     if mode == "CRD":
       # need native computation
       bsize = attrs.get("blocksize")
-      batch, channel, height, width = x.shape
+      x_shape = tf_shape(x)
+      batch, channel = x_shape[0], x_shape[1]
+      height, width = x_shape[2], x_shape[3]
       csize = channel // (bsize**2)
 
       reshape_node = tf.reshape(x, [batch, csize, bsize, bsize, height, width])
@@ -50,11 +55,12 @@ class DepthToSpace(BackendHandler):
                      [batch, csize, height * bsize, width * bsize])
       ]
 
-    else:
-      return [
-          cls.make_tensor_from_onnx_node(
-              node, attrs=attrs, c_first_cuda_only=True, **kwargs)
-      ]
+    return [
+        cls.make_tensor_from_onnx_node(node,
+                                       attrs=attrs,
+                                       c_first_cuda_only=True,
+                                       **kwargs)
+    ]
 
   @classmethod
   def version_11(cls, node, **kwargs):
