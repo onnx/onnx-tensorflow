fb1cee9678a6ff2736a89d7e81edd3c5648c4d2f
Chin Huang <chhuang@us.ibm.com>
2021-03-25 16:58:42 +0000

    Fix InstanceNormalization unknown channel size issue
    Fix InstanceNormalization unknown channel size issue where the
    reshape will fail due to unknown size. The scale shape will be
    used in this case now.
    
    This patch should fix https://github.com/onnx/onnx-tensorflow/issues/886
    
    Signed-off-by: Chin Huang <chhuang@us.ibm.com>

--- onnx_tf/handlers/backend/instance_normalization.py
+++ onnx_tf/handlers/backend/instance_normalization.py
@@ -35,8 +35,10 @@ class InstanceNormalization(BackendHandler):
     inputs_rank = inputs.shape.ndims
 
     moments_axes = list(range(inputs_rank))[2:]
-    params_shape_broadcast = list(
-        [1, inputs_shape[1]] + [1 for _ in range(2, inputs_rank)])
+    channel_size = inputs_shape[
+        1] if inputs_shape[1] is not None else gamma.shape[0]
+    params_shape_broadcast = list([1, channel_size] +
+                                  [1 for _ in range(2, inputs_rank)])
 
     beta = tf.reshape(beta, params_shape_broadcast)
     gamma = tf.reshape(gamma, params_shape_broadcast)
@@ -47,8 +49,10 @@ class InstanceNormalization(BackendHandler):
     # Compute instance normalization.
     inputs = [inputs, mean, variance, beta, gamma]
     return [
-        cls.make_tensor_from_onnx_node(
-            node, inputs=inputs, name="instancenorm", **kwargs)
+        cls.make_tensor_from_onnx_node(node,
+                                       inputs=inputs,
+                                       name="instancenorm",
+                                       **kwargs)
     ]
 
   @classmethod
