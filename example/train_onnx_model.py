fc8ac52bde43ec34f55cdaf9be53e76fd2e40772
Degao Chu <degaochu@cn.ibm.com>
2021-03-27 07:19:53 +0800

    Support training with batch normalization op; update training example (#879)
    * Support training with batch normalization op; update training example
    
    Signed-off-by: degaochu <degaochu@cn.ibm.com>
    
    * address comment for bn training with opset<7
    
    Signed-off-by: degaochu <degaochu@cn.ibm.com>
    
    Co-authored-by: Chin Huang <chhuang@us.ibm.com>

--- example/train_onnx_model.py
+++ example/train_onnx_model.py
@@ -1,12 +1,12 @@
 import os
-import onnx
 import logging
+import onnx
 import numpy as np
 
 import tensorflow as tf
-tf_compat = tf.compat.v1
-from tensorflow.keras import datasets, layers, models
+from tensorflow.keras import datasets, layers, models, regularizers
 import onnx_tf
+tf_compat = tf.compat.v1
 
 batch_size = 32
 epochs = 2
@@ -16,6 +16,7 @@ onnx_model_file = './onnx_model/model.onnx'
 trained_onnx_model = './onnx_model/trained.onnx'
 onnx_model_path = os.path.dirname(onnx_model_file)
 use_dataset = 'mnist'  # mnist or cifar10
+vgg_model = False
 
 
 def get_dataset():
@@ -38,6 +39,7 @@ def get_dataset():
 
 
 def save_trained_onnx(tensor_dict, onnx_model, sess):
+  print('Update onnx model....')
   # Collect retrained parameters.
   retrained_params = {}
   for name, tensor in tensor_dict.items():
@@ -52,43 +54,199 @@ def save_trained_onnx(tensor_dict, onnx_model, sess):
       tensor.raw_data = retrained_params[tensor.name].tobytes()
 
   onnx.save(onnx_model, trained_onnx_model)
+  print('Save trained onnx model {}'.format(trained_onnx_model))
+
+
+class VGG16(models.Model):
+
+  def __init__(self, input_shape):
+    """
+        :param input_shape: [32, 32, 3]
+        """
+    super(VGG16, self).__init__()
+
+    weight_decay = 0.000
+    self.num_classes = 10
+
+    model = models.Sequential()
+
+    model.add(
+        layers.Conv2D(64, (3, 3),
+                      padding='same',
+                      input_shape=input_shape,
+                      kernel_regularizer=regularizers.l2(weight_decay)))
+    model.add(layers.Activation('relu'))
+    model.add(layers.BatchNormalization())
+    model.add(layers.Dropout(0.3))
+
+    model.add(
+        layers.Conv2D(64, (3, 3),
+                      padding='same',
+                      kernel_regularizer=regularizers.l2(weight_decay)))
+    model.add(layers.Activation('relu'))
+    model.add(layers.BatchNormalization())
+
+    model.add(layers.MaxPooling2D(pool_size=(2, 2)))
+
+    model.add(
+        layers.Conv2D(128, (3, 3),
+                      padding='same',
+                      kernel_regularizer=regularizers.l2(weight_decay)))
+    model.add(layers.Activation('relu'))
+    model.add(layers.BatchNormalization())
+    model.add(layers.Dropout(0.4))
+
+    model.add(
+        layers.Conv2D(128, (3, 3),
+                      padding='same',
+                      kernel_regularizer=regularizers.l2(weight_decay)))
+    model.add(layers.Activation('relu'))
+    model.add(layers.BatchNormalization())
+
+    model.add(layers.MaxPooling2D(pool_size=(2, 2)))
+
+    model.add(
+        layers.Conv2D(256, (3, 3),
+                      padding='same',
+                      kernel_regularizer=regularizers.l2(weight_decay)))
+    model.add(layers.Activation('relu'))
+    model.add(layers.BatchNormalization())
+    model.add(layers.Dropout(0.4))
+
+    model.add(
+        layers.Conv2D(256, (3, 3),
+                      padding='same',
+                      kernel_regularizer=regularizers.l2(weight_decay)))
+    model.add(layers.Activation('relu'))
+    model.add(layers.BatchNormalization())
+    model.add(layers.Dropout(0.4))
+
+    model.add(
+        layers.Conv2D(256, (3, 3),
+                      padding='same',
+                      kernel_regularizer=regularizers.l2(weight_decay)))
+    model.add(layers.Activation('relu'))
+    model.add(layers.BatchNormalization())
+
+    model.add(layers.MaxPooling2D(pool_size=(2, 2)))
+
+    model.add(
+        layers.Conv2D(512, (3, 3),
+                      padding='same',
+                      kernel_regularizer=regularizers.l2(weight_decay)))
+    model.add(layers.Activation('relu'))
+    model.add(layers.BatchNormalization())
+    model.add(layers.Dropout(0.4))
+
+    model.add(
+        layers.Conv2D(512, (3, 3),
+                      padding='same',
+                      kernel_regularizer=regularizers.l2(weight_decay)))
+    model.add(layers.Activation('relu'))
+    model.add(layers.BatchNormalization())
+    model.add(layers.Dropout(0.4))
+
+    model.add(
+        layers.Conv2D(512, (3, 3),
+                      padding='same',
+                      kernel_regularizer=regularizers.l2(weight_decay)))
+    model.add(layers.Activation('relu'))
+    model.add(layers.BatchNormalization())
+
+    model.add(layers.MaxPooling2D(pool_size=(2, 2)))
+
+    model.add(
+        layers.Conv2D(512, (3, 3),
+                      padding='same',
+                      kernel_regularizer=regularizers.l2(weight_decay)))
+    model.add(layers.Activation('relu'))
+    model.add(layers.BatchNormalization())
+    model.add(layers.Dropout(0.4))
+
+    model.add(
+        layers.Conv2D(512, (3, 3),
+                      padding='same',
+                      kernel_regularizer=regularizers.l2(weight_decay)))
+    model.add(layers.Activation('relu'))
+    model.add(layers.BatchNormalization())
+    model.add(layers.Dropout(0.4))
+
+    model.add(
+        layers.Conv2D(512, (3, 3),
+                      padding='same',
+                      kernel_regularizer=regularizers.l2(weight_decay)))
+    model.add(layers.Activation('relu'))
+    model.add(layers.BatchNormalization())
+
+    model.add(layers.MaxPooling2D(pool_size=(2, 2)))
+    model.add(layers.Dropout(0.5))
+
+    model.add(layers.Flatten())
+    model.add(
+        layers.Dense(512, kernel_regularizer=regularizers.l2(weight_decay)))
+    model.add(layers.Activation('relu'))
+    model.add(layers.BatchNormalization())
+
+    model.add(layers.Dropout(0.5))
+    model.add(layers.Dense(self.num_classes))
+    # model.add(layers.Activation('softmax'))
+
+    self.model = model
+
+  def call(self, x):
+
+    x = self.model(x)
+
+    return x
 
 
 def train_tf_model():
   if use_dataset == 'mnist':
     input_shape = (28, 28, 1)
+    ds = datasets.mnist
   else:
+    ds = datasets.cifar10
     input_shape = (32, 32, 3)
-  model = models.Sequential()
-  model.add(
-      layers.Conv2D(32, (3, 3), activation='relu', input_shape=input_shape))
-  model.add(layers.MaxPooling2D((2, 2)))
-  model.add(layers.Conv2D(64, (3, 3), activation='relu'))
-  model.add(layers.MaxPooling2D((2, 2)))
-  model.add(layers.Conv2D(64, (3, 3), activation='relu'))
 
-  model.add(layers.Flatten())
-  model.add(layers.Dense(64, activation='relu'))
-  model.add(layers.Dense(10))
+  if vgg_model:
+    model = VGG16([32, 32, 3])
+    model.build(input_shape=(None, 32, 32, 3))
+  else:
+    model = models.Sequential()
+    model.add(
+        layers.Conv2D(32, (3, 3), activation='relu', input_shape=input_shape))
+    model.add(layers.MaxPooling2D((2, 2)))
+    model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+    model.add(layers.BatchNormalization())
+    model.add(layers.MaxPooling2D((2, 2)))
+    model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+    model.add(layers.BatchNormalization())
+    model.add(layers.Flatten())
+    model.add(layers.Dense(64, activation='relu'))
+    model.add(layers.BatchNormalization())
+    model.add(layers.Dense(10))
+    model.summary()
 
+  (train_images, train_labels), (test_images, test_labels) = ds.load_data()
+  train_images, test_images = train_images / 255.0, test_images / 255.0
+  if use_dataset == 'mnist':
+    train_images = train_images[..., tf.newaxis]
+    test_images = test_images[..., tf.newaxis]
+
+  model.compile(
+      optimizer='adam',
+      loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+      metrics=['accuracy'])
+
+  model.fit(train_images,
+            train_labels,
+            epochs=2,
+            validation_data=(test_images, test_labels))
+  model.evaluate(test_images, test_labels, verbose=2)
   if not os.path.exists(saved_model_path):
     os.mkdir(saved_model_path)
   model.save(saved_model_path)
-  model.summary()
   print("tf saved model: {}".format(saved_model_path))
-  '''
-    (train_images, train_labels), (test_images, test_labels) = datasets.cifar10.load_data()
-    train_images, test_images = train_images / 255.0, test_images / 255.0
-    
-    model.compile(optimizer='adam',
-              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-              metrics=['accuracy'])
-
-    history = model.fit(train_images, train_labels, epochs=4, 
-                    validation_data=(test_images, test_labels))
-    test_loss, test_acc = model.evaluate(test_images,  test_labels, verbose=2)
-    model.save(saved_model_path)
-    '''
 
 
 def convert_tf2onnx():
@@ -162,8 +320,8 @@ def train_onnx_model():
                         next_batch_value[1].flatten()
                 }
                 feed_dict[training_flag_placeholder] = False
-                loss, accuracy, _ = sess.run([loss_op, eval_op, opt_op],
-                                             feed_dict=feed_dict)
+                loss, accuracy = sess.run([loss_op, eval_op],
+                                          feed_dict=feed_dict)
                 if (step % 100) == 0:
                   print('Epoch {}, test* step {}, loss:{}, accuracy:{}'.format(
                       epoch, step, loss, accuracy))
@@ -174,26 +332,31 @@ def train_onnx_model():
       save_trained_onnx(tf_rep.tensor_dict, onnx_model, sess)
 
 
-def run_trained_onnx_model():
-  onnx_model = onnx.load(trained_onnx_model)
+def run_onnx_model(onnx_file):
+  print('Run onnx model....')
+  onnx_model = onnx.load(onnx_file)
   tf_rep = onnx_tf.backend.prepare(onnx_model, logging_level=logging.ERROR)
   input_name = tf_rep.inputs[0]
-  train_data, test_data = get_dataset()
+  _, test_data = get_dataset()
 
+  labels = []
+  preds = []
   for img, label in test_data:
-    input = img.numpy().astype('float32')
+    input_value = img.numpy().astype('float32')
     gt = label.numpy()
-    break
+    output = tf_rep.run({input_name: input_value})
+    pred = np.argmax(output[0], axis=1).tolist()
+    labels += gt.flatten().tolist()
+    preds += pred
 
-  output = tf_rep.run({input_name: input})
-
-  pred = np.argmax(output[0], axis=1).tolist()
-  print("labels:{}".format(gt.flatten().tolist()))
-  print("  pred:{}".format(pred))
+  correct_prediction = np.equal(preds, labels)
+  acc = np.mean(correct_prediction)
+  print('Accuracy: {}'.format(acc))
 
 
 if __name__ == "__main__":
   train_tf_model()
   convert_tf2onnx()
+  run_onnx_model(onnx_model_file)
   train_onnx_model()
-  run_trained_onnx_model()
+  run_onnx_model(trained_onnx_model)
