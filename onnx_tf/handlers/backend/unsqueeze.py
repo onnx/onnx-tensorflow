c7864ec05fe9e2b7386ff88a62317ced53f8702a
Pooja Krishnan <73995669+krishnanNuance@users.noreply.github.com>
2022-04-18 23:53:39 +0530

    resolve #1020 squeeze and unsqueeze opset v13 (#1022)
    * initial code for PR
    
    Signed-off-by: Pooja Krishnan <pooja.krishnan@nuance.com>
    
    * exclude backend testb based on test_sequence_map_
    
    Signed-off-by: Pooja Krishnan <pooja.krishnan@nuance.com>
    
    * 1020-removed irrelevant update in opset version
    
    Signed-off-by: Pooja Krishnan <pooja.krishnan@nuance.com>

--- onnx_tf/handlers/backend/unsqueeze.py
+++ onnx_tf/handlers/backend/unsqueeze.py
@@ -30,3 +30,27 @@ class Unsqueeze(BackendHandler):
   @classmethod
   def version_11(cls, node, **kwargs):
     return cls._common(node, **kwargs)
+
+  @classmethod
+  def version_13(cls, node, **kwargs):
+    def toUnsqueeze(x_shape, axes):
+      for ax in axes:
+        tf.autograph.experimental.set_loop_options(
+         shape_invariants=[(x_shape, tf.TensorShape([None]))])
+        if ax==-1:
+          x_shape = tf.concat([x_shape, [1]], axis=0)
+        else:
+          x_shape = tf.concat([x_shape[:ax], [1], x_shape[ax:]], axis=0)
+      return x_shape
+
+    axes = kwargs["tensor_dict"][node.inputs[1]]
+    axes = tf.sort(axes)
+
+    if axes.shape != 0:
+      x = kwargs["tensor_dict"][node.inputs[0]]
+      x_shape = tf.shape(x)
+      reshape_x = toUnsqueeze(x_shape, axes)
+      x = tf.reshape(x, reshape_x)
+      return [x]
+      
+    return [cls.make_tensor_from_onnx_node(node, **kwargs)]    
